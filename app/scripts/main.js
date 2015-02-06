fb3f59df86454feb483a75666d17717fb2c74443
Tobias Wright <tobias.wright@gmail.com>
2015-02-06 18:29:37 -0500

    added uptownUP

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,9 @@
 /* jshint devel:true */
-console.log('\'Allo \'Allo!');
+var test = UT.encrpyt('\'Allo \'Allo!', "this is my, key !!!! it is mine and no one else");
+
+console.log(test)
+
+
+var test0 = UT.decrypt( test, "this is my key it is mine and no one else");
+
+console.log(test0)
