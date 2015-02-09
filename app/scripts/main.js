dea3291e67758ed7037b3ee3fdc6bc7ea76d8889
Tobias Wright <tobias.wright@gmail.com>
2015-02-08 21:45:23 -0500

    hardcoding times data until I make it live - if ever

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,1 @@
-/* jshint devel:true */
-var test = UT.encrypt('\'Allo \'Allo!', "this is my, key !!!! it is mine and no one else");
-
-console.log(test)
-
-
-var test0 = UT.decrypt( test, "this is my key it is mine and no one else");
-
-console.log(test0)
+console.log(timesData.results)
