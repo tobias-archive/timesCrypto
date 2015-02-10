26b15b0b816fb715cc9ce1beaf4d433374bf06ef
Tobias Wright <tobias.wright@gmail.com>
2015-02-10 12:29:47 -0500

    updated bower

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,12 +5,8 @@ console.log(timesData.results);
 var phase = document.getElementById("phase"),
 	submit = document.getElementById("submit");
 
-function tooNumberArray( value ) {
-	return UT.asNumber(UT.toArray(UT.clean( value )))
-}
-
 
 submit.addEventListener("click", function(e) {
 	e.preventDefault();
-	console.log( tooNumberArray(phase.value) )
+	console.log( UT.toNumberArray(phase.value) )
 })
