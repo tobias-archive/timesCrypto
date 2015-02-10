2e0efcb697033fd74ae17c111fd719777cfd3b17
Tobias Wright <tobias.wright@gmail.com>
2015-02-10 11:27:41 -0500

    exposed a few methods from uptownCrypto

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,16 @@
 /*global timesData, console */
 
 console.log(timesData.results);
+
+var phase = document.getElementById("phase"),
+	submit = document.getElementById("submit");
+
+function tooNumberArray( value ) {
+	return UT.asNumber(UT.toArray(UT.clean( value )))
+}
+
+
+submit.addEventListener("click", function(e) {
+	e.preventDefault();
+	console.log( tooNumberArray(phase.value) )
+})
