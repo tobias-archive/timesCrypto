8aac94d4016f7449e135e59091bcc73bf84435dd
Tobias Wright <tobias.wright@gmail.com>
2015-02-11 17:04:47 -0500

    grab for elements

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,10 +3,14 @@
 console.log(timesData.results);
 
 var phase = document.getElementById("phase"),
-	submit = document.getElementById("submit");
-
+	submit = document.getElementById("submit"),
+	radio = document.getElementsByName("operation"),
+	message = document.getElementById("message");
 
 submit.addEventListener("click", function(e) {
 	e.preventDefault();
-	console.log( UT.toNumberArray(phase.value) )
+
+	console.log(radio[0].value)
+	console.log( UT.toNumberArray(phase.value) );
+	console.log( UT.encrypt(message.value, "This is my keyThis is my keyThis is my keyThis is my key" ))
 })
